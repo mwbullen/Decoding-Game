4219649cd6d016e1f3a1c1e986328aa35df670b8
mwbullen <mwbullen@gmail.com>
2015-12-08 18:06:45 -0500

    Initial Commit

---
+++ Assets/codeMessage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class codeMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	public void displayCodeMessage(string message, GameObject displayGo, GameObject letterBoxPrefab ) {
+		for (int x = 0; x < message.Length; x++){
+			char c = message[x];
+
+			GameObject lb = GameObject.Instantiate(letterBoxPrefab);
+
+			lb.name = x;
+
+
+		}
+	}
+*/
+
+}
